b26bf3fdf26b0b4ec9978f6b4615b9ea67385c8f
Oleksiy Zakharov <alexzkhr@gmail.com>
2026-06-10 06:56:40 +0300

    Modernize a little.

--- tess_svg/callbacks.h
+++ tess_svg/callbacks.h
@@ -1,13 +1,8 @@
 #ifndef CALLBACKS_H
 #define CALLBACKS_H
 
+#include <cstddef>
 #include <functional>
-#include <iostream>
-#include <map>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <typeinfo>
 
 template <typename Function>
 struct function_traits : public function_traits<decltype(&Function::operator())>
@@ -17,8 +12,8 @@ struct function_traits : public function_traits<decltype(&Function::operator())>
 template <typename ClassType, typename ReturnType, typename... Args>
 struct function_traits<ReturnType (ClassType::*)(Args...) const>
 {
-    typedef ReturnType (*pointer)(Args...);
-    typedef std::function<ReturnType(Args...)> function;
+    using pointer = ReturnType (*)(Args...);
+    using function = std::function<ReturnType(Args...)>;
 };
 
 template <typename Function>
@@ -32,19 +27,18 @@ typename function_traits<Function>::function to_function(const Function &lambda)
 {
     return static_cast<typename function_traits<Function>::function>(lambda);
 }
-typedef void (*funcptr_type)();
 
 // some more black magic
 
-template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
+template <const size_t UniqueId, typename Res, typename... ArgTypes>
 struct fun_ptr_helper
 {
   public:
-    typedef std::function<_Res(_ArgTypes...)> function_type;
+    using function_type = std::function<Res(ArgTypes...)>;
 
     static void bind(function_type &&f)
     {
-        instance().fn_.swap(f);
+        instance().fn_.swap(std::move(f));
     }
 
     static void bind(const function_type &f)
@@ -52,12 +46,12 @@ struct fun_ptr_helper
         instance().fn_ = f;
     }
 
-    static _Res invoke(_ArgTypes... args)
+    static Res invoke(ArgTypes... args)
     {
         return instance().fn_(args...);
     }
 
-    typedef decltype(&fun_ptr_helper::invoke) pointer_type;
+    using pointer_type = decltype(&fun_ptr_helper::invoke);
     static pointer_type ptr()
     {
         return &invoke;
@@ -77,20 +71,21 @@ struct fun_ptr_helper
     function_type fn_;
 };
 
-//_UniqueId must be manually set at compile time to unique value, that generates unique static stub
-
-template <const size_t _UniqueId, typename _Res, typename... _ArgTypes>
-typename fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::pointer_type
-get_fn_ptr(const std::function<_Res(_ArgTypes...)> &f)
+// UniqueId must be manually set at compile time to unique value, that generates unique static stub
+template <const size_t UniqueId, typename Res, typename... ArgTypes>
+typename fun_ptr_helper<UniqueId, Res, ArgTypes...>::pointer_type
+get_fn_ptr(const std::function<Res(ArgTypes...)> &f)
 {
-    fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::bind(f);
-    return fun_ptr_helper<_UniqueId, _Res, _ArgTypes...>::ptr();
+    fun_ptr_helper<UniqueId, Res, ArgTypes...>::bind(f);
+    return fun_ptr_helper<UniqueId, Res, ArgTypes...>::ptr();
 }
 
-template <const size_t _UniqueId, typename Function>
+using funcptr_type = void (*)();
+template <const size_t UniqueId, typename Function>
 funcptr_type to_glu_callback(const Function &lambda)
 {
-    return (funcptr_type)get_fn_ptr<_UniqueId>(to_function(lambda));
+    // NOLINTNEXTLINE
+    return reinterpret_cast<funcptr_type>(get_fn_ptr<UniqueId>(to_function(lambda)));
 }
 
 #endif // CALLBACKS_H
